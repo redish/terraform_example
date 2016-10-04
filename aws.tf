2866e2b315b3173aff26df4b9c3863324e360db5
Yusuke Yasuo <yasuo.yusuke@redish.jp>
2016-10-04 18:22:07 +0900

    amiの変更 direnvの導入 gitignoreの追加

--- aws.tf
+++ aws.tf
@@ -1,10 +1,8 @@
 provider "aws" {
-	access_key = "<access_keyを入力>"
-	secret_key = "<secret_keyを入力>"
 	region = "us-east-1"
 }
 
 resource "aws_instance" "example" {
-	ami = "ami-408c7f28"
+	ami = "ami-aa7ab6c2"
 	instance_type = "t1.micro"
 }
